c99515aadab6d62fc52bd38f11eb6dd885b5631e
R0BERT01991 <robertokepp@hotmail.com>
2018-10-02 11:11:21 -0400

    Commit

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+$(document).ready(function () { 
+        $('body').sectionScroll(); // Easy Peasy Lemon Squeezy 
+});
+
+$(window).load(function() {
+        $('#slider').nivoSlider({
+			effect:"slideInRight"
+			});
+});
